26d78d643185375dcdaa2d18bbf3bdf151f605cf
jean godoy <jean.seide@fw7.com.br>
2021-07-30 13:37:42 -0300

    WIP

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import './header.css';
 
+//icos
+import { FiPlus } from "react-icons/fi";
+
 export default function Header(prop: any)  {
 
     return (
         <header className="header">
-            Tarvos - Shorten links
+            <span>Tarvos - Shorten links</span>
+            <div className="show-all">
+            <FiPlus />
+            </div>
         </header>
     );
 }
